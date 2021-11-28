793dbb32527aae0caf43800a0f64c2915e953283
Daniel Molkentin <daniel@molkentin.de>
2021-11-28 15:50:10 +0100

    Update network settings

--- hosts/xhain/lan.nix
+++ hosts/xhain/lan.nix
@@ -2,60 +2,60 @@
 
 {
   systemd.network.networks = {
-    "40-private".networkConfig.ConfigureWithoutCarrier = true;
-    "40-public".networkConfig.ConfigureWithoutCarrier = true;
-    "40-public-nat".networkConfig.ConfigureWithoutCarrier = true;
+    "40-intern".networkConfig.ConfigureWithoutCarrier = true;
+    "40-hosting".networkConfig.ConfigureWithoutCarrier = true;
+    "40-guest".networkConfig.ConfigureWithoutCarrier = true;
   };
 
-  networking.vlans.private =    { interface = "enp4s0"; id = 42; };
-  networking.vlans.public =     { interface = "enp4s0"; id = 12; };
-  networking.vlans.public-nat = { interface = "enp4s0"; id = 37; };
+  networking.vlans.intern  = { interface = "enp4s0"; id = 42; };
+  networking.vlans.hosting = { interface = "enp4s0"; id = 37; };
+  networking.vlans.guest   = { interface = "enp4s0"; id = 12; };
 
-  networking.interfaces.private = {
-    #ipv4.addresses = [
-    #  { address = "192.168.42.0"; prefixLength = 24; }
-    #];
+  networking.interfaces.intern = {
+    ipv4.addresses = [
+      { address = "192.168.42.0"; prefixLength = 23; }
+    ];
     ipv6.addresses = [
       { address = "2a0f:5382:acab:1342::1"; prefixLength = 64; }
     ];
   };
-  networking.interfaces.public = {
+  networking.interfaces.hosting = {
     ipv4.addresses = [
       { address = "45.158.40.193"; prefixLength = 26; }
     ];
     ipv6.addresses = [
-      { address = "2a0f:5382:acab:1312::1"; prefixLength = 64; }
+      { address = "2a0f:5382:acab:1337::1"; prefixLength = 64; }
     ];
   };
-  networking.interfaces.public-nat = {
+  networking.interfaces.guest = {
     ipv4.addresses = [
-      { address = "192.168.37.0"; prefixLength = 24; }
+      { address = "192.168.12.0"; prefixLength = 23; }
     ];
     ipv6.addresses = [
-      { address = "2a0f:5382:acab:1337::1"; prefixLength = 64; }
+      { address = "2a0f:5382:acab:1312::1"; prefixLength = 64; }
     ];
   };
 
   services.radvd = {
     enable = true;
     config = ''
-      interface private {
+      interface intern {
         AdvSendAdvert on;
         prefix 2a0f:5382:acab:1342::/64 {
           AdvRouterAddr on;
         };
         RDNSS 2606:4700:4700::1111 2606:4700:4700::1001 {};
       };
-      interface public {
+      interface hosting {
         AdvSendAdvert on;
-        prefix 2a0f:5382:acab:1312::/64 {
+        prefix 2a0f:5382:acab:1337::/64 {
           AdvRouterAddr on;
         };
         RDNSS 2606:4700:4700::1111 2606:4700:4700::1001 {};
       };
-      interface public-nat {
+      interface guest {
         AdvSendAdvert on;
-        prefix 2a0f:5382:acab:1337::/64 {
+        prefix 2a0f:5382:acab:1312::/64 {
           AdvRouterAddr on;
         };
         RDNSS 2606:4700:4700::1111 2606:4700:4700::1001 {};
@@ -65,25 +65,25 @@
 
   services.dhcpd4 = {
     enable = true;
-    interfaces = [ "public" ];
+    interfaces = [ "intern" ];
     extraConfig = ''
-      #subnet 192.164.42.0 netmask 255.255.255.0 {
-      #  range 192.164.42.20 192.164.42.254;
-      #  option domain-name-servers 1.1.1.1;
-      #  option routers 192.168.42.1;
-      #  interface private;
-      #}
+      subnet 192.168.42.0 netmask 255.255.254.0 {
+        range 192.168.42.20 192.168.43.254;
+        option domain-name-servers 1.1.1.1;
+        option routers 192.168.42.1;
+        interface intern;
+      }
       subnet 45.158.40.192 netmask 255.255.255.192 {
         range 45.158.40.201 45.158.40.254;
         option domain-name-servers 1.1.1.1;
         option routers 45.158.40.193;
-        interface public;
+        interface hosting;
       }
-      subnet 192.164.13.0 netmask 255.255.255.0 {
-        range 192.164.13.20 192.164.13.254;
+      subnet 192.168.12.0 netmask 255.255.254.0 {
+        range 192.168.12.20 192.168.13.254;
         option domain-name-servers 1.1.1.1;
-        option routers 192.168.13.1;
-        interface public-nat;
+        option routers 192.168.12.1;
+        interface guest;
       }
     '';
   };
@@ -96,23 +96,23 @@
   in {
     forwardPolicy = "accept";
     extraInput = mtuFix + ''
-      iifname { private, public, public-nat } icmpv6 type nd-router-solicit accept
+      iifname { intern, hosting, guest } icmpv6 type nd-router-solicit accept
     '';
     extraOutput = mtuFix;
     extraForward = mtuFix + ''
       ct state invalid drop
       ct state established,related accept
 
-      oifname { private, public-nat } icmpv6 type { packet-too-big, echo-request, echo-reply, mld-listener-query, mld-listener-report, mld-listener-done, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept
-      oifname { private, public-nat } icmp type echo-request accept
-      oifname { private, public-nat } drop
+      oifname { intern, guest } icmpv6 type { packet-too-big, echo-request, echo-reply, mld-listener-query, mld-listener-report, mld-listener-done, nd-router-advert, nd-neighbor-solicit, nd-neighbor-advert } accept
+      oifname { intern, guest } icmp type echo-request accept
+      oifname { intern, guest } drop
     '';
     extraConfig = ''
       table ip nat {
         chain postrouting {
           type nat hook postrouting priority 100
           # masquerade private IP addresses
-          iifname { private, public-nat } snat to 45.158.40.192;
+          iifname { intern, guest } snat to 45.158.40.192;
         }
       }
     '';
