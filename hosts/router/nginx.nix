12046f6ef20f992337340d08c6864fabe18bfcb9
reimerei <git@bjrm.de>
2023-07-03 20:51:11 +0000

    disable resty interface for leases

--- hosts/router/nginx.nix
+++ hosts/router/nginx.nix
@@ -3,21 +3,21 @@
 let 
   luaFile = pkgs.copyPathToStore ./render-dhcp-leases.lua;
 in {
-  services.nginx.additionalModules = with pkgs.nginxModules; [
-    lua
-  ];
+  # services.nginx.additionalModules = with pkgs.nginxModules; [
+  #   lua
+  # ];
 
-  fileSystems."/var/lib/dhcp" = {
-    device = "/var/lib/dhcpd4";
-    options = ["bind" "ro" "X-mount.mkdir" "user" "noauto"];
-    fsType = "none";
-  };
+  # fileSystems."/var/lib/dhcp" = {
+  #   device = "/var/lib/dhcpd4";
+  #   options = ["bind" "ro" "X-mount.mkdir" "user" "noauto"];
+  #   fsType = "none";
+  # };
 
-  services.nginx.virtualHosts.${config.networking.fqdn}.locations."/leases" = {
-    extraConfig = ''
-      default_type text/html;
-      content_by_lua_file ${luaFile};
-    '';
-  };
+  # services.nginx.virtualHosts.${config.networking.fqdn}.locations."/leases" = {
+  #   extraConfig = ''
+  #     default_type text/html;
+  #     content_by_lua_file ${luaFile};
+  #   '';
+  # };
 }
 
