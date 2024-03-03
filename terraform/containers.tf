856510dae04bdbab65d4e1b5e73e73bbfeff1937
reimerei <git@bjrm.de>
2024-03-03 21:37:38 +0100

    add terraform config for proxmox

---
+++ terraform/containers.tf
@@ -0,0 +1,105 @@
+variable "token" {
+  description = "Proxmox API access token"
+}
+
+provider "proxmox" {
+  pm_api_url          = "https://pve.xhain.space:8006/api2/json"
+  pm_api_token_id     = "terraform@pve!terraform"
+  pm_api_token_secret = var.token
+}
+
+resource "proxmox_lxc" "tr33" {
+  target_node  = "pve"
+  hostname     = "tr33"
+  ostemplate   = "local:vztmpl/nixos-base.tar.xz"
+  unprivileged = true
+  cores        = 8
+  onboot       = true
+  start        = true
+  memory       = 1028
+  cmode        = "console"
+
+  ssh_public_keys = <<-EOT
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFk68ujMEgPVglDNnxqrht/0piGwofQy4GmPjgq4CvUV
+  EOT
+
+  rootfs {
+    storage = "local-zfs"
+    size    = "8G"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    ip       = "dhcp"
+    ip6      = "dhcp"
+    firewall = true
+  }
+
+  features {
+    nesting = true
+  }
+}
+
+resource "proxmox_lxc" "r33d" {
+  target_node  = "pve"
+  hostname     = "r33d"
+  ostemplate   = "local:vztmpl/nixos-base.tar.xz"
+  unprivileged = true
+  cores        = 8
+  onboot       = true
+  start        = true
+  memory       = 1028
+  cmode        = "console"
+
+  ssh_public_keys = <<-EOT
+    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFk68ujMEgPVglDNnxqrht/0piGwofQy4GmPjgq4CvUV
+  EOT
+
+  rootfs {
+    storage = "local-zfs"
+    size    = "8G"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    ip       = "dhcp"
+    ip6      = "dhcp"
+    firewall = true
+  }
+
+  features {
+    nesting = true
+  }
+}
+
+resource "proxmox_lxc" "nix-builder" {
+  target_node  = "pve"
+  hostname     = "nix-builder"
+  ostemplate   = "local:vztmpl/nixos-base.tar.xz"
+  unprivileged = true
+  cores        = 8
+  onboot       = true
+  start        = true
+  memory       = 2048
+  cmode        = "console"
+  ostype       = "nixos"
+
+  rootfs {
+    storage = "local-zfs"
+    size    = "30G"
+  }
+
+  network {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    ip       = "dhcp"
+    ip6      = "dhcp"
+    firewall = true
+  }
+
+  features {
+    nesting = true
+  }
+}
---
+++ terraform/versions.tf
@@ -0,0 +1,9 @@
+
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = ">= 2.9.14"
+    }
+  }
+}
