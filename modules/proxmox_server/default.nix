45ae22074cbf2e61fe773aaddcc5abb820d73899
reimerei <git@bjrm.de>
2024-03-03 21:58:49 +0100

    add @wheel to trusted-users

--- modules/proxmox_server/default.nix
+++ modules/proxmox_server/default.nix
@@ -31,6 +31,8 @@
   nix.gc.automatic = true;
   nix.gc.dates = lib.mkDefault "weekly";
 
+  nix.settings.trusted-users = [ "root" "@wheel" ];
+
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
 
   i18n = {
