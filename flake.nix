f0f70e118d6f32bac1f95597854de9bed02a8965
lenny <me@lenny.ninja>
2024-07-06 22:18:26 +0200

    nix nixpkgs initiation

--- flake.nix
+++ flake.nix
@@ -10,43 +10,60 @@
     kea-lease-viewer.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { nixpkgs, ... } @inputs: {
-    devShells.default = nixpkgs.mkShell {
-      packages = [
-        nixpkgs.alejandra
-        nixpkgs.git
-        nixpkgs.colmena
-      ];
-      name = "dots";
-      DIRENV_LOG_FORMAT = "";
-    };
-    colmena = {
-      meta = {
-        nixpkgs = import nixpkgs {
-          system = "x86_64-linux";
-          config = { allowUnfree = true; };
-          overlays = [ (final: prev: import ./pkgs final prev) ];
+  outputs = { nixpkgs, ... } @inputs:
+    let
+      #System types to support.
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      # Nixpkgs instantiated for supported system types.
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ (final: prev: import ./pkgs final prev) ];
+        config = { allowUnfree = true; };
+      });
+    in
+    {
+      devShell = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        pkgs.mkShell {
+          packages = with pkgs; [
+            alejandra
+            git
+            colmena
+          ];
+          name = "dots";
+          DIRENV_LOG_FORMAT = "";
+        });
+
+
+      colmena = {
+        meta = {
+          nixpkgs = nixpkgsFor."x86_64-linux";
+          specialArgs = { inherit inputs; };
         };
-        specialArgs = { inherit inputs; };
-      };
 
-      defaults = { config, lib, name, ... }: {
-        imports = [
-          (./. + "/hosts/${name}/configuration.nix")
-        ];
-        deployment.targetHost = lib.mkDefault "${name}.lan.xhain.space";
-        deployment.targetUser = null;
-      };
+        defaults = { config, lib, name, ... }: {
+          imports = [
+            (./. + "/hosts/${name}/configuration.nix")
+          ];
+          deployment.targetHost = lib.mkDefault "${name}.lan.xhain.space";
+          deployment.targetUser = null;
+        };
 
-      router = { ... }: {
-        deployment.targetHost = "router.xhain.space";
-      };
+        router = { ... }: {
+          deployment.targetHost = "router.xhain.space";
+        };
 
-      files = { ... }: {
-        deployment.targetHost = "files.xhain.space";
-      };
+        files = { ... }: {
+          deployment.targetHost = "files.xhain.space";
+        };
 
-      nix-builder = { ... }: { };
+        nix-builder = { ... }: { };
+      };
     };
-  };
 }
