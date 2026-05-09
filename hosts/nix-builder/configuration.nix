22390085aa47039758c7992c0b04e0574b4a50d6
Daniel Molkentin <daniel@molkentin.de>
2026-05-09 16:02:16 +0200

    Allow gueldi to use nix-builder

--- hosts/nix-builder/configuration.nix
+++ hosts/nix-builder/configuration.nix
@@ -23,6 +23,7 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFk68ujMEgPVglDNnxqrht/0piGwofQy4GmPjgq4CvUV reimerei@silence"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIK8wISQGiy6weaMg4tFiMHXlK71iHGoZkpfJQVNC4Up root@silence"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGiG+cRC9kMZwN/FtOmhIWfCwOjyRXbypVA6H854GW2i danimos xhain nix build key"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJoJgn7k+SHk/1u9DKNXdoQ+jdMdwWlwz0AOf1LocGHW gueldis xhain nix build key"
     ];
   };
 
